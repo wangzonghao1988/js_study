ed12f0b7f23490138427b6eea3c4f4870de74678
root <root@localhost.localdomain>
2018-04-10 10:09:40 +0800

    question 04

---
+++ chapter07/bench_xref_04.cpp
@@ -0,0 +1,59 @@
+/******************************************************************
+#
+#                    Filename: bench_xref.cpp
+#
+#                    Author: 4ever-young                      
+#                    Description: ----                          
+#             Create: 2018-04-09 12:24:14
+#
+******************************************************************/
+#include "xref.hpp"
+#include "sort.hpp"
+#include <string.h>
+
+int main()
+{
+    //call xref using split by default
+    map<string, vector<int> > ret = xref(cin);
+#if 0
+    //sort by value
+    vector<pair<string, vector<int> > > map_vector;
+    sortMapByValue(ret, map_vector); 
+    for (int i = 0; i < map_vector.size(); i++) { 
+        cout << map_vector[i].first << ":";
+        for (vector<int>::iterator j = map_vector[i].second.begin(); j != map_vector[i].second.end(); j++) {
+            cout << *j << "  ";
+        }
+        cout << endl;
+    }
+#else
+    // write the results
+    for (map<string, vector<int> >::const_iterator it = ret.begin(); it != ret.end(); ++it) {
+        // write the word
+        cout << it->first << " occurs on line(s): ";
+        // followed by one or more line numbers
+        vector<int>::const_iterator line_it = it->second.begin();
+        cout << *line_it; // write the first line number
+        ++line_it;
+        // count number
+        int count_num = 1;
+        // write the rest of the line numbers, if any
+        while (line_it != it->second.end()) {
+            if ( ++count_num % 7 == 0) {
+                count_num = 0;
+                cout << endl << string(sizeof(" occur on line(s): ") + (it->first).size(), ' ') << *line_it;
+                ++ line_it;
+            }
+            else {
+                cout << ", " << *line_it;
+                ++ line_it;
+            }
+        }
+
+    // write a new line to separate each word from the next
+        cout << endl;
+    }
+#endif
+    return 0;
+}
+
