a4a0f13125d9392a17d9d1706d51cb72ed802b5d
root <root@localhost.localdomain>
2018-04-11 14:20:22 +0800

    add 0304

---
+++ chapter09/0304/Student_info.hpp
@@ -0,0 +1,32 @@
+/******************************************************************
+#
+#                    Filename: Student_info.hpp
+#
+#                    Author: 4ever-young                      
+#                    Description: ----                          
+#             Create: 2018-04-10 19:42:39
+#
+******************************************************************/
+#include "../../head.hpp"
+
+class Student_info {
+public:
+    Student_info():midterm(0), final(0) {}
+    Student_info(istream& is) {read (is);}
+    istream& read(istream&);
+    double grade();
+    string name() const {return n;}
+    bool valid() const {return !homework.empty(); }
+
+private:
+    istream& read_hw(istream&);
+
+    string n;
+    double midterm, final;
+    vector<double> homework;
+};
+
+inline bool compare(const Student_info& x, const Student_info& y)
+{
+    return x.name() < y.name();
+}
---
+++ chapter09/0304/bench_00.cpp
@@ -0,0 +1,33 @@
+/******************************************************************
+#
+#                    Filen: bench_07.cpp
+#
+#                    Author: 4ever-young                      
+#                    Description: ----                          
+#             Create: 2018-04-10 20:01:16
+#
+******************************************************************/
+#include "Student_info.hpp"
+
+int main()
+{ 
+    vector<Student_info> students;
+    Student_info record;
+    string::size_type maxlen = 0;
+    
+    while (record.read(cin)) { // changed
+         maxlen = max(maxlen, record.name().size()); // changed
+         students.push_back(record);
+    }
+
+    sort(students.begin(), students.end(), compare);
+    for (vector<Student_info>::size_type i = 0;i != students.size(); ++i) {
+        cout << students[i].name() << string(maxlen + 1 - students[i].name().size(), ' ');
+        if (students[i].valid()) {
+            double final_grade = students[i].grade(); // changed
+            cout << final_grade;
+        }
+		cout << endl;
+    }
+    return 0;
+}
