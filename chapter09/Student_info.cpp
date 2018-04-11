bc228bcfd3cd6b7fcd6f62fd218896eef7d2fda4
root <root@localhost.localdomain>
2018-04-11 11:23:01 +0800

    update all

---
+++ chapter09/Student_info.cpp
@@ -0,0 +1,41 @@
+/******************************************************************
+#
+#                    Filename: Student_info.cpp
+#
+#                    Author: 4ever-young                      
+#                    Description: ----                          
+#             Create: 2018-04-10 19:46:13
+#
+******************************************************************/
+#include "Student_info.hpp"
+#include "grade.hpp"
+
+istream& Student_info::read(istream& is)
+{
+    is >> n >> midterm >> final;
+    read_hw(is);
+
+    return is;
+}
+
+double Student_info::grade()
+{
+    return ::grade(midterm, final, homework);
+}
+
+istream& Student_info::read_hw(istream& in)
+{
+    if (in) {
+       homework.clear();
+
+       double x;
+       int num = 0;
+       while(++num < 5 && in >> x) {
+           homework.push_back(x);
+       }
+
+       in.clear();
+    }
+    return in;
+}
+
---
+++ chapter09/grade.hpp
@@ -0,0 +1,20 @@
+/******************************************************************
+#
+#                    Filename: grade.hpp
+#
+#                    Author: 4ever-young                      
+#                    Description: ----                          
+#             Create: 2018-04-09 16:36:03
+#
+******************************************************************/
+#ifndef GUARD_grade_h
+#define GUARD_grade_h
+
+#include "../head.hpp"
+
+double grade(double, double, double);
+double grade(double, double, vector<double> &);
+
+//double grade(Student_info&);
+
+#endif
